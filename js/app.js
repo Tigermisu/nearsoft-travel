aa847f279b56ec8090d02eced705a8ece45dba3d
Unknown <tigermisu@outlook.com>
2017-03-03 18:18:29 -0700

    added js database and html view base

---
+++ js/app.js
@@ -0,0 +1,10 @@
+var $$ = Dom7;
+
+var app = new Framework7({
+    // Default title for modals
+    modalTitle: 'What\'s Next',
+});   
+
+var mainView = app.addView('.view-main', {
+    domCache: true //enable inline pages
+});
