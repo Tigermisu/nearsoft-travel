fdecaceb7a9e808959c12f26f87f859dd70529de
Alberto Flores <Alberto@Flores.cf>
2017-03-03 19:45:40 -0700

    login handler

--- js/app.js
+++ js/app.js
@@ -8,3 +8,18 @@ var app = new Framework7({
 var mainView = app.addView('.view-main', {
     domCache: true //enable inline pages
 });
+
+// login handler
+$$('.list-button').click(function() {
+    var username = $$('input[name="username"]').val();
+        var password = $$('input[name="password"]').val();
+        // Handle username and password
+    $$.each(data.users, function(i, v) {
+        if ( v.email == username && v.password == password)  {
+            mainView.router.load({pageName: 'about'});
+            alert("a");
+        }
+    });
+});
+
+
