6340feb93c68b54d26c1262e9d1b529f3aa7de43
Unknown <tigermisu@outlook.com>
2017-03-03 18:26:58 -0700

    added databse dummy data

--- js/database.js
+++ js/database.js
@@ -1,3 +1,23 @@
 var data = {
+    users: {
+        0: {
+            email: "jp@prieto.com",
+            password: "jp123"            
+        },
 
+        1: {
+            email: "lalo@dominguez.com",
+            password: "lalo123"
+        },
+
+        2: {
+            email: "admin@whatsnext.io",
+            password: "admin"
+        },
+    },
+
+    features:
+        [
+
+        ]      
 }
